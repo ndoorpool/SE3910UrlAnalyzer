25980ddfee97d3ab1002b68c7807d3a4c18207d0
kalebmm73 <kmmoore73@yahoo.com>
2025-04-04 13:39:15 -0500

    Update UrlAnalysisService.java

--- src/main/java/com/example/demo/service/UrlAnalysisService.java
+++ src/main/java/com/example/demo/service/UrlAnalysisService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
-
 import com.example.demo.domain.Url;
-import com.example.demo.repository.LogRepository;
 import org.springframework.stereotype.Service;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 
@@ -19,19 +21,25 @@ public class UrlAnalysisService {
                 return Map.of("error", "Invalid URL format");
 
             }
-            //retrieve the response code for the URL (FINISHED)
+            //retrieve the response code for the URL
             URL url1 = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
             int responseCode = connection.getResponseCode();
 
-            //retrieve the response headers (NOT FINISHED)
+            //retrieve the response headers
             Map<String, List<String>> headers = connection.getHeaderFields();
 
-            //add response header and sslcertificates once fetched from above
+            //ssl info
+            String protocol = connection.getURL().getProtocol();
+            String cipher = connection.getURL().getHost();
+
             return Map.of(
-                    "responseCode", responseCode
+                    "responseCode", responseCode,
+                    "responseHeader", headers,
+                    "protocol", protocol,
+                    "cipher", cipher
             );
 
         }
